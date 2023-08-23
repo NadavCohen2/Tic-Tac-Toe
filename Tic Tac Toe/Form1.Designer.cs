28d59e70dd65d5e397a27b1a7330644af3f1eb90
NadavCohen2 <142962719+NadavCohen2@users.noreply.github.com>
2023-08-23 07:35:46 +0300

    Add files via upload

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lab = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.startOver = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lab
+            // 
+            this.lab.AutoSize = true;
+            this.lab.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lab.Location = new System.Drawing.Point(134, 36);
+            this.lab.Name = "lab";
+            this.lab.Size = new System.Drawing.Size(84, 33);
+            this.lab.TabIndex = 0;
+            this.lab.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "3",
+            "5",
+            "7"});
+            this.comboBox1.Location = new System.Drawing.Point(123, 108);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 27);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // startOver
+            // 
+            this.startOver.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.startOver.Location = new System.Drawing.Point(387, 28);
+            this.startOver.Name = "startOver";
+            this.startOver.Size = new System.Drawing.Size(123, 49);
+            this.startOver.TabIndex = 2;
+            this.startOver.Text = "button1";
+            this.startOver.UseVisualStyleBackColor = true;
+            this.startOver.Click += new System.EventHandler(this.startOver_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 262);
+            this.Controls.Add(this.startOver);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lab);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button startOver;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox[,] board;
+        private int x, y;
+        private bool flag = true;
+        private Button b;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            b = new Button();
+            b.Width = 150;
+            b.Height = 50;
+            this.Controls.Add(b);
+            b.Click += b_Click;
+            this.Width = 400;
+            this.Height = 200;
+            b.Location = new Point(110, 20);
+            b.Text = "התחל";
+            lab.Text = "";
+            startOver.Visible = false;
+            startOver.Text = "התחל מחדש";
+
+
+
+
+        }
+
+        void b_Click(object sender, EventArgs e)
+        {
+            b.Visible = false;
+            comboBox1.Visible = false;
+            this.Width = 600;
+            this.Height = 650;
+            board = new PictureBox[int.Parse(comboBox1.Text), int.Parse(comboBox1.Text)];
+            x = 75;
+            y = 100;
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                for (int i = 0; i < board.GetLength(0); i++)
+                {
+
+                    PictureBox t = new PictureBox();
+                    t.Width = 450 / int.Parse(comboBox1.Text);
+                    t.Height = 450 / int.Parse(comboBox1.Text);
+
+                    t.Location = new Point(x, y);
+                    t.BackColor = Color.Yellow;
+                    t.BorderStyle = BorderStyle.Fixed3D;
+                    t.SizeMode = PictureBoxSizeMode.StretchImage;
+                    t.Click += t_Click;
+                    t.Tag = 0;
+                    this.Controls.Add(t);
+                    board[i, j] = t;
+                    x += 450 / int.Parse(comboBox1.Text);
+                }
+                y += 450 / int.Parse(comboBox1.Text);
+                x = 75;
+            }
+        }
+
+        void t_Click(object sender, EventArgs e)
+        {
+            bool flagche = true;
+            bool flagF = true;
+            if (((PictureBox)sender).Tag.Equals(1) || ((PictureBox)sender).Tag.Equals(2))
+            {
+                lab.Text = "המקום כבר תפוס תבחר מקום אחר";
+            }
+            else
+            {
+                if (flag == true)
+                {
+                    ((PictureBox)sender).Image = Image.FromFile("..\\..\\pic\\1.jpg");
+                    flag = false;
+                    ((PictureBox)sender).Tag = 1;
+                    lab.Text = "";
+
+                }
+                else
+                {
+                    ((PictureBox)sender).Image = Image.FromFile("..\\..\\pic\\2.jpg");
+                    flag = true;
+                    ((PictureBox)sender).Tag = 2;
+                    lab.Text = "";
+                }
+
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    flagche = true;
+                    for (int i = 0; i < board.GetLength(0); i++)
+                    {
+                        if (!(board[i, j].Tag.Equals(((PictureBox)sender).Tag)))
+                            flagche = false;
+                    }
+                    if (flagche == true)
+                    {
+                        startOver.Visible = true;
+                        flagF = false;
+                        if ((((PictureBox)sender)).Tag.Equals(1))
+                            lab.Text = "איקס ניצח";
+                        else
+                            lab.Text = "עיגול ניצח";
+                    }
+
+                }
+                if (flagF)
+                {
+                    for (int k = 0; k < board.GetLength(0); k++)
+                    {
+                        flagche = true;
+                        for (int i = 0; i < board.GetLength(1); i++)
+                        {
+                            if (!(board[k, i].Tag.Equals(((PictureBox)sender).Tag)))
+                                flagche = false;
+                        }
+
+                        if (flagche == true)
+                        {
+                            startOver.Visible = true;
+                            flagF = false;
+                            if ((((PictureBox)sender)).Tag.Equals(1))
+                                lab.Text = "איקס ניצח";
+                            else
+                                lab.Text = "עיגול ניצח";
+                        }
+                    }
+                }
+                if (flagF)
+                {
+                   flagche = true;
+                    for (int f = 0; f < board.GetLength(0); f++)
+                    { 
+                        if (!(board[f, f].Tag.Equals(((PictureBox)sender).Tag)))
+                            flagche = false;
+                    }
+                    if (flagche == true)
+                    {
+                        startOver.Visible = true;
+                        flagF = false;
+                        if ((((PictureBox)sender)).Tag.Equals(1))
+                            lab.Text = "איקס ניצח";
+                        else
+                            lab.Text = "עיגול ניצח";
+                    }
+                }
+                if (flagF)
+                {
+                    flagche = true;
+                    for (int w = 0; w < board.GetLength(0); w++)
+                    {
+                        if (!(board[w, (board.GetLength(0) - 1) - w].Tag.Equals(((PictureBox)sender).Tag)))
+                            flagche = false;
+                    }
+                    if (flagche == true)
+                    {
+                        startOver.Visible = true;
+                        flagF = false;
+                        if ((((PictureBox)sender)).Tag.Equals(1))
+                            lab.Text = "איקס ניצח";
+                        else
+                            lab.Text = "עיגול ניצח";
+                    }
+                }
+            }
+        }
+        private void startOver_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    board[i, j].Image = null;
+                    board[i, j].Tag= "";
+                }
+            }
+            lab.Text = "";
+
+        }
+    }
+}
